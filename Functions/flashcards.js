919cb889ccd86a316efb0824e9f00797911af269
Kai Tjader <141417091+KaiTjader@users.noreply.github.com>
2023-08-27 13:37:15 +0000

    completed flashcards

--- Functions/flashcards.js
+++ Functions/flashcards.js
@@ -19,18 +19,31 @@ function makeFlashcard(){
     const flashcardDiv = document.getElementById("flashcardDiv");
     const flashcardButton = document.createElement('button');
     flashcardDiv.appendChild(flashcardButton);
-    flashcardButton.className = "flashcardButton";
+    flashcardButton.className = "flashcardButtonFlipped";
     flashcardButton.id = "flashcardButton";
     flashcardButton.onclick = function flipCard() {
-        isName = !isName;
+        isImage = !isImage;
         setFlashcard();
+        if(isImage){
+            flashcardButton.className = "flashcardButtonFlipped";
+        }else{
+            flashcardButton.className = "flashcardButton";
+        }
     };
+    let front = document.createElement('div');
+    front.className = "front";
+    front.id = "front";
+    flashcardButton.appendChild(front);
     let h2 = document.createElement('h2');
     h2.className = "h2Flashcard";
     h2.id = "h2Flashcard";
-    flashcardButton.appendChild(h2);
+    front.appendChild(h2);
+    let back = document.createElement('div');
+    back.className = "back";
+    back.id = "back";
+    flashcardButton.appendChild(back);
     const image = document.createElement('image');
-    flashcardButton.appendChild(image);
+    back.appendChild(image);
     image.className = "imageFlashcard";
     image.id = "imageFlashcard";
 }
@@ -39,11 +52,7 @@ function makeArrowButtons(){
     const backButton = document.createElement('button');
     backButton.className = "arrowButtons";
     backButton.onclick = function flashcardBackward() {
-        if(index != 0){
-            index -= 1;
-            isName = true;
-            setFlashcard();
-        }
+    nextCard(0, -1);
     };
     const i1 = document.createElement('i');
     i1.className = "arrow left";
@@ -51,11 +60,7 @@ function makeArrowButtons(){
     const forwardButton = document.createElement('button');
     forwardButton.className = "arrowButtons";
     forwardButton.onclick = function flashcardForward() {
-        if(index != 43){
-            index += 1;
-            isName = true;
-            setFlashcard();
-        }
+    nextCard(43, 1);
     };
     const i2 = document.createElement('i');
     i2.className = "arrow right";
@@ -63,13 +68,28 @@ function makeArrowButtons(){
     dynamicDiv.appendChild(backButton);
     dynamicDiv.appendChild(forwardButton);
 }
+function nextCard(border, change){
+    if(index != border){
+        index += change;
+        isImage = true;
+        try {
+            const image = document.getElementById("imageFlashcard");
+            image.remove();
+          }catch (e) {
+            // catches if there was no image
+        }
+        const flashcardButton = document.getElementById("flashcardButton");
+        flashcardButton.className = "flashcardButtonFlipped";
+        setFlashcard();
+    }
+}
 let imageArray = setImages();
 let nameArray = setWords();
 let index = 0;
-let isName = true;
+let isImage = true;
 function setFlashcard(){
     const h2 = document.getElementById("h2Flashcard");
-    if(isName){
+    if(!isImage){
         try {
             const image = document.getElementById("imageFlashcard");
             image.remove();
@@ -78,12 +98,18 @@ function setFlashcard(){
           }
         h2.textContent = nameArray[index];
     }else{
-        const flashcardButton = document.getElementById("flashcardButton");
+        try {
+            const image = document.getElementById("imageFlashcard");
+            image.remove();
+          }catch (e) {
+            // catches if there was no image
+          }
+        const backDiv = document.getElementById("back");
         let image = document.createElement('img');
         image.src = "buttonImages/" + imageArray[index] + ".png";
         image.className = "imageFlashcard";
         image.id = "imageFlashcard";
-        flashcardButton.appendChild(image);
+        backDiv.appendChild(image);
         h2.textContent = "";
     }
     setPercentage();
